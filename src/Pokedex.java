6fd7ea649b065894daa0be1c843de118b8a09372
Yaneth Millan <yanethmillanalcocer@gmail.com>
2026-08-13 22:35:00 -0600

    Create Pokedex class and initialize ArrayList
    Created Pokedex.java with a private ArrayList<Pokemon> field to store local Pokemon objects and initialized it inside the constructor.

---
+++ src/Pokedex.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Pokedex {
+    private List<Pokemon> pokemonList;
+    
+    public Pokedex(){
+        this.pokemonList = new ArrayList<>();
+    }
+}
